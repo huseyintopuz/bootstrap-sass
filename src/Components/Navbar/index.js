999236cc981bf744df58ae75f9956df5c11392b0
huseyintopuz <huseyin.topuz35@gmail.com>
2022-09-18 23:23:01 +0300

    bootstrap

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const index = () => {
+const Index = () => {
+    const [visible, setVisible] = useState(false)
     return (
         <nav class="navbar navbar-expand-lg navbar-dark menu">
             <div class="container">
                 <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler"
+                    onClick={() => setVisible(!visible)}
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -21,4 +30,4 @@ const index = () => {
     )
 }
 
-export default index
+export default Index
